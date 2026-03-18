6d417db19e5a5a89a3f74024b156a45091dd4637
Chris <sammilergood@gmail.com>
2026-03-18 16:39:10 +0800

    Use Qt6_DIR and windeployqt for Windows deployment (#1)

--- cmake/copy_qt_platforms.cmake
+++ cmake/copy_qt_platforms.cmake
@@ -2,77 +2,49 @@ if(NOT WIN32)
   return()
 endif()
 
-if(NOT NOTES_DEPLOY_QT_PLATFORMS)
-  return()
+if(NOT DEFINED NOTES_WINDEPLOYQT OR NOTES_WINDEPLOYQT STREQUAL "")
+  message(FATAL_ERROR "NOTES_WINDEPLOYQT was not provided.")
 endif()
 
-set(_notes_qt_platform_candidates
-    "${NOTES_QT_BIN_DIR}/../Qt6/plugins/platforms"
-    "${NOTES_QT_BIN_DIR}/../plugins/platforms")
-set(_notes_qt_qml_candidates
-    "${NOTES_QT_BIN_DIR}/../Qt6/qml"
-    "${NOTES_QT_BIN_DIR}/../qml")
-set(_notes_qt_runtime_bin_candidates
-    "${NOTES_QT_BIN_DIR}"
-    "${NOTES_QT_BIN_DIR}/../bin")
-
-set(NOTES_QT_PLATFORMS_SOURCE "")
-foreach(_notes_candidate IN LISTS _notes_qt_platform_candidates)
-  cmake_path(NORMAL_PATH _notes_candidate OUTPUT_VARIABLE _notes_candidate)
-  if(EXISTS "${_notes_candidate}")
-    set(NOTES_QT_PLATFORMS_SOURCE "${_notes_candidate}")
-    break()
-  endif()
-endforeach()
-
-if(NOT NOTES_QT_PLATFORMS_SOURCE)
-  message(WARNING "Qt platforms directory was not found from ${NOTES_QT_BIN_DIR}")
-  return()
+if(NOT EXISTS "${NOTES_WINDEPLOYQT}")
+  message(FATAL_ERROR "windeployqt.exe was not found: ${NOTES_WINDEPLOYQT}")
 endif()
 
-set(NOTES_QT_QML_SOURCE "")
-foreach(_notes_candidate IN LISTS _notes_qt_qml_candidates)
-  cmake_path(NORMAL_PATH _notes_candidate OUTPUT_VARIABLE _notes_candidate)
-  if(EXISTS "${_notes_candidate}")
-    set(NOTES_QT_QML_SOURCE "${_notes_candidate}")
-    break()
-  endif()
-endforeach()
+set(_notes_deploy_args
+    --dir
+    "${NOTES_QT_RUNTIME_DEST}"
+    --qmldir
+    "${NOTES_QML_SOURCE_DIR}"
+    --no-translations
+    --force)
 
-file(MAKE_DIRECTORY "${NOTES_QT_PLATFORMS_DEST}")
-file(COPY "${NOTES_QT_PLATFORMS_SOURCE}/"
-     DESTINATION "${NOTES_QT_PLATFORMS_DEST}")
-
-message(STATUS "Copied Qt platforms to ${NOTES_QT_PLATFORMS_DEST}")
-
-if(NOTES_QT_QML_SOURCE)
-  file(MAKE_DIRECTORY "${NOTES_QT_QML_DEST}")
-  file(COPY "${NOTES_QT_QML_SOURCE}/" DESTINATION "${NOTES_QT_QML_DEST}")
-  message(STATUS "Copied Qt qml modules to ${NOTES_QT_QML_DEST}")
+if(NOTES_DEPLOY_CONFIGURATION STREQUAL "Debug")
+  list(APPEND _notes_deploy_args --debug)
 else()
-  message(WARNING "Qt qml directory was not found from ${NOTES_QT_BIN_DIR}")
+  list(APPEND _notes_deploy_args --release)
 endif()
 
-set(NOTES_QT_RUNTIME_BIN_SOURCE "")
-foreach(_notes_candidate IN LISTS _notes_qt_runtime_bin_candidates)
-  cmake_path(NORMAL_PATH _notes_candidate OUTPUT_VARIABLE _notes_candidate)
-  if(EXISTS "${_notes_candidate}")
-    set(NOTES_QT_RUNTIME_BIN_SOURCE "${_notes_candidate}")
-    break()
-  endif()
-endforeach()
+execute_process(
+  COMMAND "${NOTES_WINDEPLOYQT}" ${_notes_deploy_args} "${NOTES_TARGET_FILE}"
+  RESULT_VARIABLE _notes_windeployqt_result
+  OUTPUT_VARIABLE _notes_windeployqt_stdout
+  ERROR_VARIABLE _notes_windeployqt_stderr)
 
-if(NOTES_QT_RUNTIME_BIN_SOURCE)
-  file(GLOB _notes_qt_runtime_dlls "${NOTES_QT_RUNTIME_BIN_SOURCE}/Qt6*.dll")
-  if(_notes_qt_runtime_dlls)
-    file(COPY ${_notes_qt_runtime_dlls} DESTINATION "${NOTES_QT_RUNTIME_DEST}")
-    message(STATUS "Copied Qt runtime dlls to ${NOTES_QT_RUNTIME_DEST}")
-  else()
-    message(WARNING "No Qt runtime dlls found in ${NOTES_QT_RUNTIME_BIN_SOURCE}")
-  endif()
-else()
-  message(WARNING "Qt runtime bin directory was not found from ${NOTES_QT_BIN_DIR}")
+if(_notes_windeployqt_stdout)
+  string(STRIP "${_notes_windeployqt_stdout}" _notes_windeployqt_stdout)
+  message(STATUS "${_notes_windeployqt_stdout}")
 endif()
 
-file(WRITE "${NOTES_QT_CONF_PATH}" "[Paths]\nPlugins = platforms\nQmlImports = qml\n")
+if(_notes_windeployqt_stderr)
+  string(STRIP "${_notes_windeployqt_stderr}" _notes_windeployqt_stderr)
+  message(STATUS "${_notes_windeployqt_stderr}")
+endif()
+
+if(NOT _notes_windeployqt_result EQUAL 0)
+  message(
+    FATAL_ERROR
+      "windeployqt failed with exit code ${_notes_windeployqt_result}.")
+endif()
+
+file(WRITE "${NOTES_QT_CONF_PATH}" "[Paths]\nPlugins = .\nQmlImports = qml\n")
 message(STATUS "Wrote Qt runtime config to ${NOTES_QT_CONF_PATH}")
